913160cfafd8afcc52c4bd47acaa4566b3dd8b56
javi_bavi <xavierwood3737@gmail.com>
2024-03-31 16:14:42 -0500

    Switching to pyside. This is the last time we are switching

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-import tkinter as tk
+from PySide6.QtWidgets import QApplication, QLabel
+import sys
 
 
-class Gui:
+app = QApplication(sys.argv)
+label = QLabel("Hello world")
 
-    def __init__(self) -> None:
-        self.window = tk.Tk()
-
-        self.window.geometry(
-            f"{int(self.window.winfo_screenwidth()/1.5)}x{int(self.window.winfo_screenheight()/1.5)}"
-        )
-
-        self.window.config(bg="#222222")
-
-        self.window.title("Discreet Dial")
-
-        self.entry = tk.Text(self.window, bg="#222222", fg="#FFFFFF", width=int(self.window.winfo_screenwidth()/1.5), font=("Courier", 30), height=5)
-        self.entry.pack(side="bottom")
-
-    def loop(self) -> None:
-        self.window.mainloop()
-
-
-root = Gui()
-
-root.loop()
+label.show()
+app.exec()
