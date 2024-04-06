658077c5b286332b6edaed520faae6124713d774
javi_bavi <xavierwood3737@gmail.com>
2024-04-06 16:11:00 -0500

    Fixed issues with geometry, added another text box for output

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ if __name__ == "__main__":
     # Create the Qt Application
     app = QApplication(sys.argv)
 
+    # Gets the height and width of the screen that the app is on and stores them as constants
+    # This is used to determine the geometry of the window upon opening
+    SCREEN_HEIGHT = app.primaryScreen().size().height()
+    SCREEN_WIDTH = app.primaryScreen().size().width()
+
     # Declare our window subclass and show it
-    window = Window()
+    window = Window(SCREEN_HEIGHT, SCREEN_WIDTH)
     window.show()
+
+    print(f'{SCREEN_HEIGHT}, {SCREEN_WIDTH}')
 
     # Run the main Qt loop
     sys.exit(app.exec())
--- window.py
+++ window.py
@@ -1,36 +1,56 @@
-from PySide6.QtWidgets import (QVBoxLayout, QMainWindow, QScrollArea,
-                               QSizePolicy, QSpacerItem, QTextEdit, QWidget)
+from PySide6.QtWidgets import (QMainWindow, QScrollArea, QTextEdit,
+                               QVBoxLayout, QWidget)
+
 
 # Subclass of QMainWindow used to create a window tailored exactly how we want it
 class Window(QMainWindow):
-    def __init__(self):
+    def __init__(self, height: int, width: int):
         super().__init__()
 
+        # Creates constants for us based on the parameters passed to our subclass
+        self.HEIGHT, self.WIDTH = height, width
+
         # Basic settings for title of window, color, and geometry
-        # TODO: Figure out a way to set geometry based on size of monitor (like tkinter screenwidth() function)
+        # The geometry settings should make it appear roughly in the center of any screen, regardless of resolution
         self.setWindowTitle("Discreet Dial")
         self.setStyleSheet("background-color: #222222;")
-        self.setGeometry(300, 200, 1000, 800)
+        self.setGeometry(
+            int(self.WIDTH / 4),
+            int(self.HEIGHT / 4),
+            int(self.WIDTH / 2),
+            int(self.HEIGHT / 2),
+        )
 
         # Create the central widget and set it as the central widget of our class
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
 
         # Create a layout for the central widget
+        # This ensures that our text boxes will stack themselves on top of each other
         self.central_layout = QVBoxLayout(self.central_widget)
 
-        # Add a stretchable spacer to push the text to the bottom (temp solution hopefully)
-        # TODO: Change this to be another QTextEdit that will be unmodifiable to the user and will print necessary outputs
-        self.spacer = QSpacerItem(20, 400, QSizePolicy.Minimum, QSizePolicy.Expanding)
-        self.central_layout.addItem(self.spacer)
+        # This is our output for text chat
+        # We give it a stretch factor of 2 to ensure that it is bigger than our entry box
+        self.output = QTextEdit()
+        self.output.setFontFamily("JetBrains Mono")
+        self.output.setFontPointSize(20)
+        self.output.setMinimumHeight(int(self.HEIGHT / 2.1))
+        self.output.setReadOnly(True)
 
-        # Create a QTextEdit for text entry
-        self.text_entry = QTextEdit()
-        self.text_entry.setFontFamily("Courier")
+        # Create a QScrollArea to make the output text scrollable and adds our output box to the scroll area
+        self.output_scroll_area = QScrollArea()
+        self.output_scroll_area.setWidgetResizable(True)
+        self.output_scroll_area.setWidget(self.output)
+        self.central_layout.addWidget(self.output_scroll_area, 7)
 
-        # Create a QScrollArea to make the text area scrollable
-        self.scroll_area = QScrollArea()
-        self.scroll_area.setWidgetResizable(True)
-        self.scroll_area.setWidget(self.text_entry)
-        self.central_layout.addWidget(self.scroll_area)
+        # This is the input for text chat
+        self.input = QTextEdit()
+        self.input.setFontFamily("JetBrains Mono")
+        self.input.setFontPointSize(20)
+        self.input.setStyleSheet("border: 1px solid white")
 
+        # Create a QScrollArea to make the input text area scrollable and adds our text entry to it
+        self.input_scroll_area = QScrollArea()
+        self.input_scroll_area.setWidgetResizable(True)
+        self.input_scroll_area.setWidget(self.input)
+        self.central_layout.addWidget(self.input_scroll_area, 2)
