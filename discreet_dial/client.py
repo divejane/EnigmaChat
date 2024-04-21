f2c0c5a6a7b22bbbf11d25592e39b483d131688c
divejane <sonny.nichols5689@gmail.com>
2024-04-20 23:18:14 -0500

    v0.8.13 Public IP support (work in progress TCP hole punch)

--- discreet_dial/client.py
+++ discreet_dial/client.py
@@ -1,9 +1,13 @@
 # TODO: p2p encrypt & nat hole punch: these are the last things to do before 1.0 release
+# TODO: the serverside code for socketio is for the most part done, still need to do socketio client. Also need to actually do the threading/async
+# for the hole punch. Try to make it only return one socket when the whole operation is over.
 
 from requests import get, post, delete, exceptions
 from threading import Thread
-import socket
-from util.util import *
+import socketio  # websocket
+import socket  # standard socket
+import util
+import tcp_hole_punch
 
 HOST = "https://discreetdial.ddns.net/"
 
@@ -13,12 +17,12 @@ def cli_menu() -> None:
     conf = [
         "anon"]  # This will eventually be put into a dynamically generated 'config' file
     while True:
-        cli_cls()
-        cli_draw_logo()
+        util.cli_cls()
+        util.cli_draw_logo()
 
         print("1) join a room\n2) create a room\n3) settings\n")
 
-        point = valid_usinp(3)
+        point = util.valid_usinp(3)
 
         if point == 1:
             room_list_load(conf)
@@ -30,8 +34,8 @@ def cli_menu() -> None:
 
 def generate_room(conf) -> None:
     """user generates room, room info is json'ed, sent off to server"""
-    cli_cls()
-    cli_draw_logo()
+    util.cli_cls()
+    util.cli_draw_logo()
 
     while True:
         roomname = input("enter room name: ")
@@ -45,25 +49,27 @@ def generate_room(conf) -> None:
                          'is_password_protected': len(password) > 0,
                          'host_port': host_sock.getsockname()[1]}
 
-            room_id = (post(HOST + 'rooms', data=host_info))
+            waitlist_sock = socketio.SimpleClient()
+            waitlist_sock.connect(HOST, transports=['websocket'])
+            room_id = waitlist_sock.emit('addroom', host_info)
             print('\nattempting to connect to server...')
             break
         print("room name and/or password must be between 0 and 16 characters\n")
     print("room configured")
-    room_host_load(host_sock, room_id)
+    room_host_load(host_sock, room_id, waitlist_sock)
 
 
 def room_list_load(conf: list) -> None:
     """requests rooms from server, formats them, checks if selected rooms has a password, then sends the room_id + password to server to be
     checked"""
-    cli_cls()
-    cli_draw_logo()
+    util.cli_cls()
+    util.cli_draw_logo()
 
     open_rooms = get(HOST + 'rooms')
 
-    if open_rooms.text == 'nohosts':
+    if open_rooms.text == 'no open rooms':
         print('no available rooms (0 to exit)\n')
-        usinp = valid_usinp(0)
+        usinp = util.valid_usinp(0)
         if usinp == 0:
             return
 
@@ -80,7 +86,7 @@ def room_list_load(conf: list) -> None:
     print("\n~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
     print("\nenter room to join (0 to exit) -")
 
-    usinp = valid_usinp(len(open_rooms))
+    usinp = util.valid_usinp(len(open_rooms))
 
     if usinp == 0:
         return
@@ -99,7 +105,7 @@ def room_list_load(conf: list) -> None:
         room_connect.raise_for_status()
     except exceptions.HTTPError:
         print('password was rejected (0 to exit)')
-        if valid_usinp(0) == 0:
+        if util.valid_usinp(0) == 0:
             return
     except AttributeError:
         join_room_load(room_connect, conf, roomid_to_room['username'])
@@ -107,29 +113,31 @@ def room_list_load(conf: list) -> None:
 
 def join_room_load(room_connect: dict, conf: list, peer_username: str) -> None:
     """attempts to connect to localhost on port, and connects to public ip if that fails. sends username on socket init"""
-    cli_cls()
-    cli_draw_logo()
+    util.cli_cls()
+    util.cli_draw_logo()
 
     print("sent host connection info...")
     peer_join_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         peer_join_sock.connect(("0.0.0.0", int(room_connect['host_port'])))
     except ConnectionRefusedError:
-        peer_join_sock.connect(
-            (room_connect['host_ip'], int(room_connect['host_port'])))
+        peer_join_sock.bind(("0.0.0.0", room_connect[1]+1))
+        peer_join_sock = tcp_hole_punch.h_punch(
+            peer_join_sock, room_connect[0], room_connect[1])
     peer_join_sock.sendall(conf[0].encode())
     print("\nconnection successful, please wait...")
     chatroom(peer_join_sock, peer_username)
 
 
-def room_host_load(host_sock: object, room_id: str) -> None:
+def room_host_load(host_sock: object, room_id: str, waitlist_sock: object) -> None:
     """listens until client connects, then sends off a room_id to the server so that the room can be deleted"""
-    cli_cls()
-    cli_draw_logo()
+    util.cli_cls()
+    util.cli_draw_logo()
 
     print("\n\nroom configured, awaiting peer establishment...")
-    host_sock.listen()
-    conn, addr = host_sock.accept()
+    peer_ip = waitlist_sock.recieve()
+    conn = tcp_hole_punch.h_punch(
+        host_sock, peer_ip, host_sock.getsockname()[1]+1)
     confirm_connect = delete(HOST + 'rooms', data={'room_id': room_id})
     print("\npeer connection successful, awaiting further info...")
     peer_username = conn.recv(1024).decode()
@@ -154,8 +162,8 @@ def conn_read(peer_connection: object, peer_username: str) -> None:
 
 def chatroom(peer_connection: object, peer_username: str) -> None:
     """handles outgoing messages and closing the room socket on disconnect"""
-    cli_cls()
-    cli_draw_logo()
+    util.cli_cls()
+    util.cli_draw_logo()
 
     print(f"connected to {peer_username}, type '/exit' to leave")
     # daemon because god knows this thing is going to crash
@@ -182,13 +190,13 @@ def chatroom(peer_connection: object, peer_username: str) -> None:
 def cli_settings(conf: list) -> list:
     """user configuration of conf"""
     while True:
-        cli_cls()
-        cli_draw_logo()
+        util.cli_cls()
+        util.cli_draw_logo()
 
         print(f'1) change username ("{conf[0]}")\n2) back')
-        usinp = valid_usinp(2)
+        usinp = util.valid_usinp(2)
         if usinp == 1:
-            conf[0] = change_username()
+            conf[0] = util.change_username()
         elif usinp == 2:
             break
     return conf
---
+++ discreet_dial/tcp_hole_punch.py
@@ -0,0 +1,27 @@
+import socket
+import asyncio
+
+
+async def punch_send(peer_ip: str, peer_port: int) -> object:
+    hp_send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        try:
+            await hp_send_sock.connect((peer_ip, peer_port))
+        except ConnectionRefusedError:
+            continue
+        return hp_send_sock
+
+
+def punch_recv(sock: object, peer_ip: str, peer_port: int) -> object:
+    punch_send_thr = punch_send(peer_ip, peer_port)
+    while punch_send_thr is None:
+        try:
+            conn, addr = sock.accept()
+        except socket.timeout:
+            continue
+        return conn
+    return punch_send_thr
+
+
+def h_punch(sock: object, peer_ip: str, peer_port: int):
+    working_socket = asyncio.run(punch_recv())
