69fd6c87e334389b30d8a464eb27381ead0e27cd
divejane <sonny.nichols5689@gmail.com>
2024-04-22 23:44:58 -0500

    v0.8.13a Public IP test

--- discreet_dial/tcp_hole_punch.py
+++ discreet_dial/tcp_hole_punch.py
@@ -9,13 +9,14 @@ def punch_send(peer_ip: str, peer_port: int) -> object:
     hp_send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     hp_send_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     hp_send_sock.bind(('0.0.0.0', peer_port))
-    hp_send_sock.setblocking(0)
+    hp_send_sock.settimeout(3)
     while not stop.is_set():
         try:
             hp_send_sock.connect((peer_ip, peer_port))
         except:
             print('restarting sender...')
             continue
+        print('punch_send finished')
         stop.set()
         working_sock.append(hp_send_sock)
 
@@ -34,6 +35,7 @@ def punch_recv(sock: object, peer_ip: str, peer_port: int) -> object:
         except socket.timeout:
             print('restarting listener...')
             continue
+        print('punch_recv finished')
         stop.set()
         working_sock.append(conn)
     return working_sock[0]
