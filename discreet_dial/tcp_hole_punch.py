0c73c06e3800aab924d665757561568abbd416bc
divejane <sonny.nichols5689@gmail.com>
2024-04-22 23:42:34 -0500

    v0.8.13a Public IP test

--- discreet_dial/tcp_hole_punch.py
+++ discreet_dial/tcp_hole_punch.py
@@ -7,23 +7,25 @@ stop = Event()
 
 def punch_send(peer_ip: str, peer_port: int) -> object:
     hp_send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    hp_send_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     hp_send_sock.bind(('0.0.0.0', peer_port))
+    hp_send_sock.setblocking(0)
     while not stop.is_set():
-        # try:
-        hp_send_sock.connect((peer_ip, peer_port))
-        # except:
-        #     print('restarting sender...')
-        #     continue
-        # stop.set()
-        # working_sock.append(hp_send_sock)
+        try:
+            hp_send_sock.connect((peer_ip, peer_port))
+        except:
+            print('restarting sender...')
+            continue
+        stop.set()
+        working_sock.append(hp_send_sock)
 
 
 def punch_recv(sock: object, peer_ip: str, peer_port: int) -> object:
-    print(peer_ip, peer_port)
     print("started holepunch")
     punch_send_thr = Thread(
         target=punch_send, args=(peer_ip, peer_port), daemon=True)
     punch_send_thr.start()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.settimeout(5)
     sock.listen()
     while not stop.is_set():
